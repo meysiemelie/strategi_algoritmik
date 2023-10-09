376b0c0222a0285d62368f696e01d291bc063964
meystamelania <141213769+meysiemelie@users.noreply.github.com>
2023-10-09 13:08:58 +0700

    Add files via upload

---
+++ Meysta Amelania menghitung waktu 1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+
+// Headers
+string toString (double);
+int toInt (string);
+double toDouble (string);
+
+int main() {
+    double jarak;
+    int s;
+    int v;
+    double sinA;
+
+    cin >> s;
+    cin >> v;
+    sinA = "";
+    jarak = "";
+    cout << jarak << endl;
+    return 0;
+}
+
+// The following implements type conversion functions.
+string toString (double value) { //int also
+    stringstream temp;
+    temp << value;
+    return temp.str();
+}
+
+int toInt (string text) {
+    return atoi(text.c_str());
+}
+
+double toDouble (string text) {
+    return atof(text.c_str());
+}
+
